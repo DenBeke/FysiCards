cd37d53018bddc84062760656f585361fe195dcc
MathiasB <git@denbeke.be>
2015-03-16 21:36:32 +0100

    [FIX] constraints in structure.sql

--- structure.sql
+++ structure.sql
@@ -3,20 +3,6 @@
 SET NAMES utf8mb4;
 SET time_zone = '+00:00';
 
-DROP TABLE IF EXISTS `answer`;
-CREATE TABLE `answer` (
-  `id` int(32) NOT NULL AUTO_INCREMENT,
-  `user` int(10) unsigned NOT NULL,
-  `question` int(16) unsigned NOT NULL,
-  `answer` varchar(64) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `user` (`user`),
-  KEY `question` (`question`),
-  CONSTRAINT `answer_ibfk_1` FOREIGN KEY (`user`) REFERENCES `user` (`id`),
-  CONSTRAINT `answer_ibfk_2` FOREIGN KEY (`question`) REFERENCES `question` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
 DROP TABLE IF EXISTS `question`;
 CREATE TABLE `question` (
   `id` int(16) unsigned NOT NULL AUTO_INCREMENT,
@@ -38,5 +24,17 @@ CREATE TABLE `user` (
   UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+DROP TABLE IF EXISTS `answer`;
+CREATE TABLE `answer` (
+  `id` int(32) NOT NULL AUTO_INCREMENT,
+  `user` int(10) unsigned NOT NULL,
+  `question` int(16) unsigned NOT NULL,
+  `answer` varchar(64) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user` (`user`),
+  KEY `question` (`question`),
+  CONSTRAINT `answer_ibfk_1` FOREIGN KEY (`user`) REFERENCES `user` (`id`),
+  CONSTRAINT `answer_ibfk_2` FOREIGN KEY (`question`) REFERENCES `question` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- 2015-03-13 13:15:40
